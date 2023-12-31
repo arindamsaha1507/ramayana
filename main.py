a8f34ed1149a72bef9177abefcf1b8c34efeed45
Arindam Saha <arindamsaha1507@gmail.com>
2023-12-31 09:28:07 -0500

    Added erratum

--- main.py
+++ main.py
@@ -47,24 +47,33 @@ def main():
 
     # data = DataExtractor.download_data()
 
-    primary_word_list = pd.read_csv("primary_word_list.csv", header=None)[0].tolist()
+    primary_word_df = pd.read_csv("primary_word_list.csv", header=None)
+    primary_word_list = primary_word_df[0].tolist()
+    avayava_list = primary_word_df[primary_word_df[2] == "Linga.AVAYAVA"][0].tolist()
+
+    erratum_df = pd.read_csv("erratum.csv", header=None)
+    erratum = erratum_df[0].tolist()
+
+    avayava_list.extend(erratum_df[erratum_df[2] == "Linga.AVAYAVA"][0].tolist())
+    primary_word_list.extend(erratum)
+
     primary_word_set = set(primary_word_list)
+
     secondary_word_list = pd.read_csv("secondary_word_list.csv", header=None)[
         0
     ].tolist()
+    secondary_word_list.extend(avayava_list)
+
     secondary_word_set = set(secondary_word_list)
     verb_word_list = pd.read_csv("verb_word_list.csv", header=None)[0].tolist()
     verb_word_set = set(verb_word_list)
 
     kanda = 1
     sarga = 1
-    section = 1
+    section = 2
 
     with open(f"text/padas/{kanda}_{sarga}_padas.yml", "r", encoding="utf-8") as file:
         data = yaml.safe_load(file)[section]
-
-    # print(data[2])
-    # print(word_set)
 
     for index, word in enumerate(data):
         print_search_results(
